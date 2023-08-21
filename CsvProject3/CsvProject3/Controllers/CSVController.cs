3d1d0006b173afc4a458a15dda8ebad31f9536f9
1sirojiddin <109944871+1sirojiddin@users.noreply.github.com>
2023-08-22 01:15:15 +0500

    Update CSVController.cs

--- CsvProject3/CsvProject3/Controllers/CSVController.cs
+++ CsvProject3/CsvProject3/Controllers/CSVController.cs
@@ -8,45 +8,42 @@ using System.Globalization;
 
 namespace CsvProject3.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class CsvController : Controller
+[ApiController]
+[Route("[controller]")]
+public class CsvController : Controller
+{
+    private readonly UserService _userService;
+    private readonly DataContext _dataContext;
+    public CsvController(UserService userService, DataContext dataContext)
     {
-        private readonly UserService _userService;
-        private readonly DataContext _dataContext;
-        public CsvController(UserService userService, DataContext dataContext)
-        {
-            _userService = userService;
-            _dataContext = dataContext;
-        }
-        
-        [HttpPost("parse-file")]
-        public async Task<IActionResult> UploadExcel(IFormFile file)
-        {
-            if (file == null || file.Length == 0)
-                return BadRequest("File is empty.");
+         _userService = userService;
+        _dataContext = dataContext;
+    } 
+    [HttpPost("parse-file")]
+    public async Task<IActionResult> UploadExcel(IFormFile file)
+    {
+        if (file == null || file.Length == 0)
+            return BadRequest("File is empty.");
 
-            using (var stream = new MemoryStream())
-            {
-                await file.CopyToAsync(stream);
-                stream.Position = 0;
-
-                await _userService.ProcessExcelFile(stream);
-            }
-
-            return Ok("File processed successfully.");
-        }
-        [HttpGet("Sorting")]
-        public IActionResult GetUsersSortedByUsername([FromQuery] bool isAscending = true)
+        using (var stream = new MemoryStream())
         {
-            var sortedUsers = _userService.GetUsersSortedByUsername(isAscending);
-            return Ok(sortedUsers);
+            await file.CopyToAsync(stream);
+            stream.Position = 0;
+            await _userService.ProcessExcelFile(stream);
         }
-        [HttpGet("Limitations")]
-        public async Task<ActionResult<IEnumerable<User>>> GetUsers(int maxCount = 10)
-        {
-            var users = await _dataContext.Users.Take(maxCount).ToListAsync();
-            return Ok(users);
+        return Ok("File processed successfully.");
         }
+    [HttpGet("Sorting")]
+    public IActionResult GetUsersSortedByUsername([FromQuery] bool isAscending = true)
+    {
+        var sortedUsers = _userService.GetUsersSortedByUsername(isAscending);
+        return Ok(sortedUsers);
     }
+    [HttpGet("Limitations")]
+    public async Task<ActionResult<IEnumerable<User>>> GetUsers(int maxCount = 10)
+    {
+        var users = await _dataContext.Users.Take(maxCount).ToListAsync();
+        return Ok(users);
+    }
+}
 }
