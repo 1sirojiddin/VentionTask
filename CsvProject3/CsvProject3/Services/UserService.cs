3c5a49ddf331286c8a30e8b67ae050b5e77bb964
1sirojiddin <109944871+1sirojiddin@users.noreply.github.com>
2023-08-22 01:11:23 +0500

    Update UserService.cs

--- CsvProject3/CsvProject3/Services/UserService.cs
+++ CsvProject3/CsvProject3/Services/UserService.cs
@@ -6,54 +6,44 @@ using System;
 
 namespace CsvProject3.Services
 {
-    public class UserService
+public class UserService
+{
+    private readonly DataContext _context;
+    public UserService(DataContext context)
     {
-        private readonly DataContext _context;
-
-        public UserService(DataContext context)
-        {
-            _context = context;
-        }
-
+        _context = context;
+    }
         public async Task ProcessExcelFile(Stream fileStream)
         {
-            using (var package = new ExcelPackage(fileStream))
+        using (var package = new ExcelPackage(fileStream))
+        {
+            var worksheet = package.Workbook.Worksheets[0]; // Assuming data is in the first worksheet
+            for (int row = 2; row <= worksheet.Dimension.Rows; row++) // Start from the second row assuming headers in the first row
             {
-                var worksheet = package.Workbook.Worksheets[0]; // Assuming data is in the first worksheet
-
-                for (int row = 2; row <= worksheet.Dimension.Rows; row++) // Start from the second row assuming headers in the first row
+                var Id = worksheet.Cells[row, 1].Value.ToString();
+                var Username = worksheet.Cells[row, 2].Value.ToString();
+                var UserIdentifier = worksheet.Cells[row, 3].Value.ToString();
+                var Age = worksheet.Cells[row, 4].Value.ToString();
+                var City = worksheet.Cells[row, 5].Value.ToString();
+                var PhoneNumber = worksheet.Cells[row, 6].Value.ToString();
+                var Email = worksheet.Cells[row, 7].Value?.ToString();
+                if (int.TryParse(Id, out int id) && int.TryParse(Age, out int age))
                 {
-                    var Id = worksheet.Cells[row, 1].Value.ToString();
-                    var Username = worksheet.Cells[row, 2].Value.ToString();
-                    var UserIdentifier = worksheet.Cells[row, 3].Value.ToString();
-                    var Age = worksheet.Cells[row, 4].Value.ToString();
-                    var City = worksheet.Cells[row, 5].Value.ToString();
-                    var PhoneNumber = worksheet.Cells[row, 6].Value.ToString();
-                    var Email = worksheet.Cells[row, 7].Value?.ToString();
-
-                    if (int.TryParse(Id, out int id) &&
-          int.TryParse(Age, out int age))
+                    var existingUser = _context.Users.FirstOrDefault(u => u.Id == id);
+                    if (existingUser != null)
                     {
-                        var existingUser = _context.Users.FirstOrDefault(u => u.Id == id);
-
-                        if (existingUser != null)
-                        {
-                            existingUser.Username = Username;
-                            existingUser.Age = age;
-                            existingUser.Email = Email;
-                        }
-                        else
-                        {
-                            _context.Users.Add(new User { Id = id, Username = Username, UserIdentifier = UserIdentifier, Age = age, City = City, PhoneNumber = PhoneNumber, Email = Email });
-                        }
+                        existingUser.Username = Username;
+                        existingUser.Age = age;
+                        existingUser.Email = Email;
                     }
                     else
                     {
-                        // Handle parsing errors here
+                        _context.Users.Add(new User { Id = id, Username = Username, UserIdentifier = UserIdentifier, Age = age, City = City, PhoneNumber = PhoneNumber, Email = Email });
                     }
+                    }
+                else{}
                 }
-
-                await _context.SaveChangesAsync();
+            await _context.SaveChangesAsync();
             }
         }
         public IEnumerable<User> GetUsersSortedByUsername(bool isAscending )
@@ -68,7 +58,6 @@ namespace CsvProject3.Services
             {
                 query = query.OrderByDescending(u => u.Username);
             }
-
             return query.ToList();
         }
     }
