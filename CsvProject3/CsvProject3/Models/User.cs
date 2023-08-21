a9800eb72bed9c8a427935c6accbe77c7b58cc64
1sirojiddin <109944871+1sirojiddin@users.noreply.github.com>
2023-08-22 01:03:16 +0500

    Update User.cs

--- CsvProject3/CsvProject3/Models/User.cs
+++ CsvProject3/CsvProject3/Models/User.cs
@@ -2,15 +2,15 @@
 
 namespace CsvProject3.Models
 {
-    public class User
-    {
-        [Key]
-        public int Id { get; set; }
-        public string Username { get; set; }
-        public string UserIdentifier { get; set; }
-        public int Age { get; set; }
-        public string City { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Email { get; set; }
-    }
+public class User
+{
+    [Key]
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string UserIdentifier { get; set; }
+    public int Age { get; set; }
+    public string City { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+}
 }
